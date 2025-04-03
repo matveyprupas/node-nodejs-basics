23a73d261d88dbc050098c394db5871fb23c7d33
Matvey Prupas <mapr@gurtam.team>
2025-04-03 22:44:26 +0300

    fix: implement delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.resolve('fs/files/fileToRemove.txt');
+
+    try {
+        await fs.rm(filePath);
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await remove();
