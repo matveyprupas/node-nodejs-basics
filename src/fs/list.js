817830ebbe3b760b71520a7efbe378d36f011f02
Matvey Prupas <mapr@gurtam.team>
2025-04-03 22:50:46 +0300

    feat: implement list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
-};
+    const dirPath = path.resolve('fs/files');
+
+    try {
+        const dirList = await fs.readdir(dirPath, {withFileTypes: true, recursive: true });
+
+        console.log(dirList.map((item) => item.name));
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }};
 
 await list();
