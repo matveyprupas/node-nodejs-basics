cb7a7c6320ad8de6295732cf44da587728104950
Matvey Prupas <mapr@gurtam.team>
2025-04-03 22:38:23 +0300

    feat: implement rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const oldPath = path.resolve('fs/files/wrongFilename.txt');
+    const newPath = path.resolve('fs/files/properFilename.md');
+    const errorMessage = 'FS operation failed';
+
+    try {
+        await fs.access(newPath);
+        throw new Error(errorMessage);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            try {
+                await fs.rename(oldPath, newPath);
+            } catch (err) {
+                throw new Error(errorMessage);
+            }
+        } else {
+            throw error;
+        }
+    }      
 };
 
 await rename();
