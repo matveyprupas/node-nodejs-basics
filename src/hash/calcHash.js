0c2d6a4b6335c647285ef3c2217ea8d7857e48ee
Matvey Prupas <mapr@gurtam.team>
2025-04-06 17:24:43 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import crypto from 'crypto';
+import path from 'path';
+import fs from 'fs/promises';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = path.resolve('hash/files/fileToCalculateHashFor.txt');
+    const fileInfo = await fs.readFile(filePath)
+
+    const hash = crypto.createHash('sha256').update(fileInfo).digest('hex');
+
+    console.log(hash);
 };
 
 await calculateHash();
