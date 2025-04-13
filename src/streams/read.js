2ddfdbaae5ebeddbc4436f2d7fb697dfbcdecabf
Matvey Prupas <mapr@gurtam.team>
2025-04-13 21:49:39 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import path from 'path';
+import fs from 'fs';
+
 const read = async () => {
-    // Write your code here 
+    const srcPath = path.resolve('streams/files/fileToRead.txt');
+    
+    const readable = fs.createReadStream(srcPath, {encoding: 'utf-8'});
+
+    readable.on('data', (chunk) => {
+        process.stdout.write(`${chunk}\n`);
+    })
 };
 
 await read();
