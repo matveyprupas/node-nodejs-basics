1643905b4c8f2c406cee82cf1a28b999f5972a71
Matvey Prupas <mapr@gurtam.team>
2025-04-13 22:16:35 +0300

    feat: implement transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
-const transform = async () => {
-    // Write your code here 
-};
+import { pipeline, Transform } from 'stream';
 
+const transform = async () => {
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const transformedChunk = chunk.toString().split('').filter(el => el !== '\n').reverse().join('');
+            callback(null, `${transformedChunk}\n`);
+        }
+    })
+
+    pipeline(
+        process.stdin, 
+        transformStream,
+        process.stdout, 
+        (err) => {
+            console.log(err);
+        }
+    );
+}
 await transform();
