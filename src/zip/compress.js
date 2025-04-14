c9f224aafdf17c5db5ba9886df4a0a43d803bec9
Matvey Prupas <mapr@gurtam.team>
2025-04-14 22:26:31 +0300

    feat: implement zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import { pipeline } from 'stream';
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const readPath = path.resolve('zip/files/fileToCompress.txt');
+    const writePath = path.resolve('zip/files/archive.gz');
+    
+    const readableStream = fs.createReadStream(readPath);
+    const writeableStream = fs.createWriteStream(writePath);
+    const gzip = zlib.createGzip();
+
+    pipeline(readableStream, gzip, writeableStream, (err) => {
+        console.log(err);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import { pipeline } from 'stream';
+import fs from 'fs';
+import path from 'path';
+import zlib from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const readPath = path.resolve('zip/files/archive.gz');
+    const writePath = path.resolve('zip/files/fileToCompress.txt');
+    
+    const readableStream = fs.createReadStream(readPath);
+    const writeableStream = fs.createWriteStream(writePath);
+    const unzip = zlib.createGunzip();
+
+    pipeline(readableStream, unzip, writeableStream, (err) => {
+        console.log(err);
+    });
 };
 
 await decompress();
