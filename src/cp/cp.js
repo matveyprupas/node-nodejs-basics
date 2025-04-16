f2f60fd8f5852a82f5d8d3eba06efccc866bc72a
Matvey Prupas <mapr@gurtam.team>
2025-04-16 22:21:36 +0300

    feat: implement CP

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const spawnedProcess = spawn('node', ['cp/files/script.js', ...args], {
+        stdio: ['pipe', 'pipe', 'pipe'],
+    });
+
+    spawnedProcess.stdout.on('data', (data) => {
+        console.log(`Child process stdout: ${data}`);
+    });
+    spawnedProcess.stderr.on('data', (data) => {
+        console.error(`Child process stderr: ${data}`);
+    });
+    spawnedProcess.on('close', (code) => {
+        console.log(`Child process exited with code ${code}`);
+    });
+    spawnedProcess.on('error', (error) => {
+        console.error(`Error spawning child process: ${error}`);
+    });
+
+    process.stdin.pipe(spawnedProcess.stdin);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 'someArgument3']);
